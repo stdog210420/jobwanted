b17fc1104ec6bdf94d96f8c017ca6460f73149cb
Phoebe <hsingyinlin@gmail.com>
2024-12-08 18:45:26 +0800

    Initial commit for jobwanted

--- week03/scripts/form.js
+++
@@ -1,9 +0,0 @@
-// get all selected values from multi-select
-let selected = genres.options[genres.selectedIndex];
-alert(selected.value); // blues
-
-let newOption = new Option("Classic", "classic");
-genres.append(newOption);
-
-// 3)
-newOption.selected = true;
